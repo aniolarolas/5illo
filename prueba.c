89b44e57ddc111369d4b2d4d45b5bdc7472973e9
GerardMiBa <gerard.minarro.ballester@estudiantat.upc.edu>
2024-11-24 18:53:55 +0100

    Update prueba.c

--- prueba.c
+++ prueba.c
@@ -117,6 +117,136 @@ printf("¿Quieres ver todas las cartas? [s/n] ");
 scanf("%c%*c",&carta_pregunta);
 printf("\n");
 
+//
+if(jug=='S'||jug=='s'){
+		if(carta_pregunta=='S'||carta_pregunta=='s'){
+			printf("     ");
+			for(i=1;i<10;i++){
+				printf("[ %d ]",i);
+			}
+			for(i=10;i<11;i++){
+				printf("[%d ]",i);
+			}
+			printf("\n");
+			printf("J#1: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug1.mano[i].palo,jug1.mano[i].numero2,jug1.mano[i].numero);
+			}
+			printf("\n");
+			printf("J#2: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug2.mano[i].palo,jug2.mano[i].numero2,jug2.mano[i].numero);
+			}
+			printf("\n");
+			printf("J#3: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug3.mano[i].palo,jug3.mano[i].numero2,jug3.mano[i].numero);
+			}
+			printf("\n");
+			printf("J#4: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug4.mano[i].palo,jug4.mano[i].numero2,jug4.mano[i].numero);
+			}
+			printf("\n\n");
+		}
+		else if(carta_pregunta=='N'||carta_pregunta=='n'){
+			printf("     ");
+			for(i=1;i<10;i++){
+				printf("[ %d ]",i);
+			}
+			for(i=10;i<11;i++){
+				printf("[%d ]",i);
+			}
+			printf("\n");
+			printf("J#1: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug1.mano[i].palo,jug1.mano[i].numero2,jug1.mano[i].numero);
+			}
+			printf("\n");
+			printf("J#2: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug2_auxiliar.mano[i].palo,jug2_auxiliar.mano[i].numero2,jug2_auxiliar.mano[i].numero);
+			}
+			printf("\n");
+			printf("J#3: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug3_auxiliar.mano[i].palo,jug3_auxiliar.mano[i].numero2,jug3_auxiliar.mano[i].numero);
+			}
+			printf("\n");
+			printf("J#4: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug4_auxiliar.mano[i].palo,jug4_auxiliar.mano[i].numero2,jug4_auxiliar.mano[i].numero);
+			}
+			printf("\n\n");
+		}
+	}
+	else if(jug=='N'||jug=='n'){
+		if(carta_pregunta=='S'||carta_pregunta=='s'){
+			printf("     ");
+			for(i=1;i<10;i++){
+				printf("[ %d ]",i);
+			}
+			for(i=10;i<11;i++){
+				printf("[%d ]",i);
+			}
+			printf("\n");
+			printf("J#1: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug1.mano[i].palo,jug1.mano[i].numero2,jug1.mano[i].numero);
+			}
+			printf("\n");
+			printf("J#2: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug2.mano[i].palo,jug2.mano[i].numero2,jug2.mano[i].numero);
+			}
+			printf("\n");
+			printf("J#3: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug3.mano[i].palo,jug3.mano[i].numero2,jug3.mano[i].numero);
+			}
+			printf("\n");
+			printf("J#4: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug4.mano[i].palo,jug4.mano[i].numero2,jug4.mano[i].numero);
+			}
+			printf("\n\n");
+ 		}
+		else if(carta_pregunta=='N'||carta_pregunta=='n'){
+			printf("     ");
+			for(i=1;i<10;i++){
+				printf("[ %d ]",i);
+			}
+			for(i=10;i<11;i++){
+				printf("[%d ]",i);
+			}
+			printf("\n");
+			printf("J#1: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug1_auxiliar.mano[i].palo,jug1_auxiliar.mano[i].numero2,jug1_auxiliar.mano[i].numero);
+			}
+			printf("\n");
+			printf("J#2: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug2_auxiliar.mano[i].palo,jug2_auxiliar.mano[i].numero2,jug2_auxiliar.mano[i].numero);
+			}
+			printf("\n");
+			printf("J#3: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug3_auxiliar.mano[i].palo,jug3_auxiliar.mano[i].numero2,jug3_auxiliar.mano[i].numero);
+			}
+			printf("\n");
+			printf("J#4: ");
+			for(i=0;i<10;i++){
+				printf("[%c%c%c]",jug4_auxiliar.mano[i].palo,jug4_auxiliar.mano[i].numero2,jug4_auxiliar.mano[i].numero);
+			}
+			printf("\n\n");
+		}
+	}
+
+
+
+
+	
 //Cartas por jugador iniciales
     printf("J#1: ");
     for(i=0;i<10;i++){
