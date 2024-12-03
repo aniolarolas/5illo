57f83bfa79e71c34d0ef2c6b5ea9ed44415351a1
GerardMiBa <gerard.minarro.ballester@estudiantat.upc.edu>
2024-12-03 15:40:35 +0100

    Update partida.h

--- partida.h
+++ partida.h
@@ -4,7 +4,9 @@
 #include "estructuras.h"
 
 void inicializar_partida(t_jugador *jug1,t_jugador *jug2,t_jugador *jug3,t_jugador *jug4,t_jugador *jug1_auxiliar,t_jugador *jug2_auxiliar,t_jugador *jug3_auxiliar,t_jugador *jug4_auxiliar,t_baraja *baraja);
-void inicializar_tablero(t_mesa *mesa,t_jugador *jug1,t_jugador *jug2,t_jugador *jug3,t_jugador *jug4);
+void inicializar_tablero(t_mesa *mesa);
+void imprimir_tablero(t_mesa mesa,t_jugador *jug1,t_jugador *jug2,t_jugador *jug3,t_jugador *jug4);
+
 
 
 #endif
