40c373481b907f3b168d877454c9388d0c4676ca
GerardMiBa <gerard.minarro.ballester@estudiantat.upc.edu>
2024-12-08 17:22:24 +0100

    Update partida.h

--- partida.h
+++ partida.h
@@ -7,7 +7,7 @@ void inicializar_partida(t_jugador *jug1,t_jugador *jug2,t_jugador *jug3,t_jugad
 void inicializar_tablero(t_mesa *mesa);
 void imprimir_tablero(t_mesa mesa);
 void buscar_tirades_posibles(t_mesa mesa,t_mesa mesa_auxiliar,t_jugador *tiradas_posibles);
-void comparar_cartas_jugador(t_jugador jug,t_jugador tirades_posibles,t_jugador *tiradas_escoger)
+void comparar_cartas_jugador(t_jugador jug,t_jugador tirades_posibles,t_jugador *tiradas_escoger);
 
 
 #endif
